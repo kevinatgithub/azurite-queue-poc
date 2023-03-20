5f11db4b7bc9a54553dc36aa236638bf4f1ad78b
Kevin <933595@cognizant.com>
2023-03-20 15:32:42 +0800

    init

---
+++ Sender.API/Sender.API/Controllers/QueueController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sender.API.Services;
+
+namespace Sender.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class QueueController : ControllerBase
+{
+    private readonly QueueService _queueService;
+
+    public QueueController(QueueService queueService)
+    {
+        _queueService = queueService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMessageAsync(string message)
+    {
+        await _queueService.SendMessageAsync(message);
+
+        return Ok();
+    }
+}
---
+++ Sender.API/Sender.API/Services/QueueService.cs
@@ -0,0 +1,43 @@
+﻿using Azure.Storage.Queues.Models;
+using Azure.Storage.Queues;
+
+namespace Sender.API.Services;
+
+public class QueueService
+{
+    private readonly QueueClient _queueClient;
+
+    public QueueService()
+    {
+        _queueClient = new QueueClient("UseDevelopmentStorage=true;", "app1-queue");
+    }
+
+    public async Task SendMessageAsync(string message)
+    {
+        await _queueClient.CreateIfNotExistsAsync();
+        var encoded = Base64Encode(message);
+        await _queueClient.SendMessageAsync(encoded);
+    }
+
+    public async Task<string> ReceiveMessageAsync()
+    {
+        QueueMessage[] messages = await _queueClient.ReceiveMessagesAsync(maxMessages: 1);
+
+        if (messages.Length == 0)
+        {
+            return null;
+        }
+
+        QueueMessage message = messages[0];
+
+        await _queueClient.DeleteMessageAsync(message.MessageId, message.PopReceipt);
+
+        return message.MessageText;
+    }
+
+    private string Base64Encode(string plainText)
+    {
+        var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+        return System.Convert.ToBase64String(plainTextBytes);
+    }
+}
